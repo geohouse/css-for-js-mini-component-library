9fe3686e0c10abf63f0331910caed90c37405c19
Geoffrey House <geo.house7@gmail.com>
2022-12-13 16:35:02 -0700

    select done

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -11,37 +11,64 @@ const Select = ({ label, value, onChange, children }) => {
   console.log(children);
   console.log(displayedValue);
   return (
-    <>
+    <Wrapper>
+      <DefaultSelect value={value} onChange={onChange}>
+        {children}
+      </DefaultSelect>
       <CustomSelect>
-        <DefaultSelect value={value} onChange={onChange}>
-          {children}
-        </DefaultSelect>
         {displayedValue}
-        <Icon
-          id="chevron-down"
-          style={{ display: "inline-block", padding: "18px 16px" }}
-        />
+        <IconWrapper>
+          <Icon id="chevron-down" strokeWidth={1} size={24} />
+        </IconWrapper>
       </CustomSelect>
-    </>
+    </Wrapper>
   );
 };
 
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+  isolation: isolate;
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0px;
+  right: 10px;
+  bottom: 0px;
+  margin: auto;
+  width: 24px;
+  height: 24px;
+  z-index: -1;
+  /* This can be used instead of setting z index to make this element transparent to any click events, instead
+  transmitting them to the underlying layer, which is the (transparent) select menu element.*/
+  /* pointer-events: none; */
+`;
+
 const DefaultSelect = styled.select`
-  /* opacity: 0; */
+  opacity: 0;
   position: absolute;
   top: 0;
   left: 0;
+  height: 100%;
+  width: 100%;
+  appearance: none;
 `;
 
 const CustomSelect = styled.div`
-  position: relative;
-  width: fit-content;
+  color: ${COLORS.gray700};
   background-color: ${COLORS.transparentGray15};
+  font-size: ${16 / 16}rem;
   border-radius: 8px;
-  padding: 16px 12px;
-  margin-top: 0;
-  margin-bottom: 0;
-  line-height: 1.5;
+  padding: 12px 52px 12px 16px;
+  ${DefaultSelect}:focus + & {
+    outline: 5px auto -webkit-focus-ring-color;
+    outline: 5px auto Highlight;
+  }
+
+  ${DefaultSelect}:hover + & {
+    color: ${COLORS.black};
+  }
 `;
 
 export default Select;
