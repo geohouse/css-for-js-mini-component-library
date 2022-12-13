61f07608fa3001e95abee8b47fcc4b7c7ff8d7cf
Geoffrey House <geo.house7@gmail.com>
2022-12-13 14:13:31 -0700

    re-work as div to start

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -7,28 +7,64 @@ import VisuallyHidden from "../VisuallyHidden";
 
 const styles = {
   small: {
-    height: "10px",
+    height: "8px",
   },
   medium: {
-    height: "20px",
+    height: "12px",
   },
   large: {
-    height: "40px",
+    height: "24px",
+    padding: "4px",
   },
 };
 
 const ProgressBar = ({ value, size }) => {
   const appliedStyle = styles[size];
+  appliedStyle["--width"] = value + "%";
   return (
-    <>
-      <VisuallyHidden for="progress-bar">Label</VisuallyHidden>
-      <Bar id="progress-bar" value="0" style={appliedStyle}></Bar>
-    </>
+    <div
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+    >
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <BarHolder
+        style={
+          size === "large"
+            ? { "border-radius": "8px", padding: "4px" }
+            : { "border-radius": "4px" }
+        }
+      >
+        <Bar style={appliedStyle} />
+      </BarHolder>
+    </div>
   );
 };
 
-const Bar = styled.progress`
-  background-color: red;
+const Bar = styled.div`
+  width: var(--width);
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
 `;
+
+const BarHolder = styled.div`
+  width: 100;
+  box-shadow: inset 0px 2px 4px rgba(128, 128, 128, 0.35);
+  background: ${COLORS.transparentGray15};
+`;
+
+// const Bar = styled.progress`
+//   background-color: ${COLORS.transparentGray15};
+
+//   border: none;
+//   ::-moz-progress-bar {
+//     background-color: ${COLORS.primary};
+//     border-radius: 4px 0 0 4px;
+//   }
+//   ::-webkit-progress-inner-element {
+//     box-shadow: inset 0px 2px 4px rgba(128, 128, 128, 0.35);
+//   }
+// `;
 
 export default ProgressBar;
