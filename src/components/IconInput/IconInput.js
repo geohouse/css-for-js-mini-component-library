e945f1d3964bd10479e1562294756be4f5a9f842
Geoffrey House <geo.house7@gmail.com>
2022-12-13 17:03:26 -0700

    Start on icon

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  return (
+    <Wrapper style={{ width: width + "px" }}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <Icon id={icon} style={{ display: "inline-block" }}></Icon>
+      <TextInput placeholder={placeholder}></TextInput>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  border: none;
+  border-bottom: 1px solid ${COLORS.black};
+`;
+
+const TextInput = styled.input.attrs((props) => ({
+  type: "text",
+  id: "input-text",
+  placeholder: props.placeholder,
+}))`
+  border: none;
+`;
 
 export default IconInput;
